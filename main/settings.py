bb2320934890f1215daf751f4c6c153db0e69ed1
iso <ismailkurtlojistik@gmail.com>
2022-12-28 11:42:55 +0300

    decouple

--- main/settings.py
+++ main/settings.py
@@ -38,11 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # myapps
+    # myapp
     'todo',
-    # thirtpart app
-    'rest_framework',
+
+    # third party apps
+    'rest_frame_work',
 
 ]
 
