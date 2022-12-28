1a159771af7e26895974945e308e30e0905bbbbd
iso <ismailkurtlojistik@gmail.com>
2022-12-28 11:32:14 +0300

    commit

---
+++ todo/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import (
+    todo_list_create,
+    todo_home,
+    todo_detail,
+    Todos,
+    TodoDetail,
+    TodoMVS,
+)
+
+router = routers.DefaultRouter()
+router.register('todo', TodoMVS)
+
+urlpatterns = [
+    path('', todo_home),
+    # path('list-create/', todo_list_create),
+    # path('detail/<int:id>/', todo_detail),
+    path('list-create/', Todos.as_view()),
+    path('detail/<int:pk>/', TodoDetail.as_view()),
+    path('', include(router.urls))
+
+]
